b6653751f9dbce62249f3c6813555d9d1adfd451
matheuslcandido <matheus.l.candido@gmail.com>
2019-06-13 19:47:28 -0300

    connecting to the database mongo dbInstagramClone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://dbInstagramClone:iqOvp9cAdHVKebYE@cluster0-fpxxy.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+});
 
 app.get('/', (req, res) => {
     return res.send(`Hello ${req.query.name}!`);
